beaf2492b9dfb1244a36f6498b626f5ac64b0c1a
zhanying <1569526994@qq.com>
2024-04-08 09:38:05 +0800

    format

--- Project-for-intro-CS-main/Project-for-intro-CS-main/SHAO/双向链表.py
+++ Project-for-intro-CS-main/Project-for-intro-CS-main/SHAO/双向链表.py
@@ -1,19 +1,28 @@
-lst = ['TOTAL_STATION', 'STRATEGY', 'DISTANCE', 'end',
-       'clock', 'counterclockwise', 'clockwise', 'target']
+lst = [
+    "TOTAL_STATION",
+    "STRATEGY",
+    "DISTANCE",
+    "end",
+    "clock",
+    "counterclockwise",
+    "clockwise",
+    "target",
+]
 
 
 class Bus(object):
-    native_space = 'Bus:'
+    native_space = "Bus:"
+
     def __init__(self, item):
-        self.tar = '0000000000'
-        self.wise = '0000000000'
-        self.counterclockwise = '0000000000'
+        self.tar = "0000000000"
+        self.wise = "0000000000"
+        self.counterclockwise = "0000000000"
 
 
 class Station(object):
     def __init__(self, position):
-        self.position = f'position:{str(position)}'
-        self.sta = 'STATION:'
+        self.position = f"position:{str(position)}"
+        self.sta = "STATION:"
         self.next = None
         self.prev = None
         self.mark = -1
@@ -48,8 +57,8 @@ class Roundlink(object):
     def travel(self):
         cur = self._head
         while cur != None:
-            print(cur.position, end='     ')
-            if (cur.mark != -1):
+            print(cur.position, end="     ")
+            if cur.mark != -1:
                 print(cur.mark)
             cur = cur.next
 
@@ -57,7 +66,7 @@ class Roundlink(object):
         cur = self._head
         for _ in range(i):  # 找到那个结点
             cur = cur.next
-        cur.mark = ((i+1)//cnt)
+        cur.mark = (i + 1) // cnt
 
 
 global run
@@ -65,13 +74,13 @@ run = Roundlink()
 
 
 def create(a, b):
-    for i in range(a*b):
+    for i in range(a * b):
         run.append(i)
-        if (i+1) % b == 0:
+        if (i + 1) % b == 0:
             run.Mark(i, b)
 
 
-with open("C:\\Users\\15695\\Desktop\\新建 文本文档 (2).txt", 'rt') as f:
+with open("C:\\Users\\15695\\Desktop\\新建 文本文档 (2).txt", "rt") as f:
     lst2 = [f.readline() for _ in range(3)]
     if lst[0] in lst2[0] or lst[0] in lst2[1] or lst[0] in lst[2]:
         print(1)
